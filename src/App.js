8ce33a4e1d58c61a3ab38863b475a4c0b7ebe90a
Yasmina Gavaldà <y.gavalda@gmail.com>
2017-10-23 19:38:42 +0200

    Add input search videos

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react'
 import VideoList from './Components/VideoList'
 import axios from 'axios'
 
+
 class App extends Component {
 
 	constructor(props) {
 		super(props)
 
 		this.state = {
+			inputValue: "",
 			videos: []
 		}
 
-		this.initialize()
+		this.search()
 	}
 
-	initialize() {
+	search(query) {
 
-		const query = `radiohead`
 	  const url = `https://www.googleapis.com/youtube/v3/search?maxResults=10&part=snippet&q=${query}&key=AIzaSyCiOQBjMZ8vE8l07wF5F96XgU8cRG2tbGk`
 
 	  const promise = axios.get(url)
@@ -35,13 +36,20 @@ class App extends Component {
 
   render() {
   	return (
-	    <VideoList videos={this.state.videos} />
+  		<div>
+	  		<input 
+	  			value={this.state.inputValue} 
+	  			onChange={(event) => {
+	  				console.log(event)
+	  				this.setState( { inputValue: event.target.value} )}}/> 
+	  		<button 
+	  			onClick={() => this.search(this.state.inputValue) }>
+	  			Search
+	  		</button>
+		    <VideoList videos={this.state.videos} />
+	    </div>
 	  )
   }
-
-  
-  
-
   
 }
 
